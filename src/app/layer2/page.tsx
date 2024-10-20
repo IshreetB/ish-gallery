f950ff7383f90542ee9c3ca9210bcb2f419bb868
ishreetb <ishreetb@gmail.com>
2024-10-20 03:58:42 -0400

    scaffold ui

---
+++ src/app/layer2/page.tsx
@@ -0,0 +1,3 @@
+export default function Layer2() {
+  return <main className="">(Page for layout 2)</main>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/1dea96aa-ffa4-4b46-95fe-f171c27727c2-1d.jpeg",
+  "https://utfs.io/f/410eba9b-25f9-4d04-b22a-5e95ec2d382f-1e.jpeg",
+  "https://utfs.io/f/e564e79c-3d34-4bc2-a051-fc1a8a2550de-1f.jpeg",
+  "https://utfs.io/f/22a9340e-1576-4b01-9e23-c61f5df257a1-1g.jpeg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Test Gallery
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="image" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
