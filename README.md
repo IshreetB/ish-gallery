86210a866ec18bcde9eb258f38fe40d5d5fd83a8
ishreetb <ishreetb@gmail.com>
2024-10-23 14:16:54 -0400

    used auth for homepage

--- README.md
+++ README.md
@@ -8,6 +8,7 @@
 - [x] Attach database to UI
 - [x] Add authentication (w/ Clerk)
 - [ ] Add image upload
+- [ ] "taint" (server-only)
 - [ ] Error management (w/ Sentry)
 - [ ] Routing/image page (parallel routes)
 - [ ] Delete Button (w/ server actions)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,36 @@
 import Link from "next/link";
 import { db } from "../server/db";
+import { SignedOut, SignedIn } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
+    <div className="flex flex-wrap gap-4">
+      {[...images].map((image, index) => (
+        <div key={image.id + "-" + index} className="flex w-48 flex-col">
+          <img src={image.url} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default function HomePage() {
+  return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...images].map((image, index) => (
-          <div key={image.id + "-" + index} className="flex w-48 flex-col">
-            <img src={image.url} />
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
